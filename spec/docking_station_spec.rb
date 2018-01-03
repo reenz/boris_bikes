c3c78ad8da3bc0861d76bc7974dab3082910fcfb
ealitten <ea.litten@gmail.com>
2018-01-03 15:13:09 +0000

    docking station releases working bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,13 +1,11 @@
 require "docking_station"
 
 # Longer syntax:
-#describe DockingStation do
-#  it "shoud release bike" do
-#    expect(DockingStation).to respond_to :release_bike
-#  end
-#end
-
-
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+
+  it "should release working bike" do
+      bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end

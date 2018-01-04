48b68df5baeaa7015260ce334e44b141e9aaff1a
reena <reena.sanjay.sharma@gmail.com>
2018-01-04 12:43:50 +0000

    raises error when its full

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,13 +9,14 @@ class DockingStation
 
   def release_bike
     if @docked_bikes.length == 0
-      raise "No bikes"
+      raise "No bike"
     else
-    return  @docked_bikes.pop
+      return @docked_bikes.pop
     end
   end
 
   def dock_bike(bike)
+    raise "Its full" if @docked_bikes.length == 1
     @docked_bikes << bike
     @docked_bikes.last #return last stored bike, to check bike was stored correctly
   end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,9 +2,11 @@ require "docking_station"
 
 describe DockingStation do
 
-  #it "should release bike" do
-  #  expect(subject.release_bike).to be_a Bike
-  #end
+  it "should release bike" do
+    bike = Bike.new
+    subject.dock_bike(bike)
+   expect(subject.release_bike).to be_a Bike
+  end
 
   it "should dock bike in station" do
     bike = Bike.new
@@ -14,7 +16,14 @@ describe DockingStation do
   it "should show bikes in station" do
     expect(subject.docked_bikes).to eq([])
   end
+
   it "raises error" do
-    expect{subject.release_bike}.to raise_error
+    expect{subject.release_bike}.to raise_error "No bike"
   end
+  it "raises error when full" do
+    bike1 = Bike.new
+    subject.dock_bike(bike1)
+    bike2 = Bike.new
+  expect{subject.dock_bike(bike2)}.to raise_error "Its full"
+end
 end
